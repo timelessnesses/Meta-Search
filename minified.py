c9d0c45ab2d5753415300eac2949211a7f95704b
Rukchad Wongprayoon <contact@biomooping.tk>
2021-11-10 09:09:49 +0700

    Update minified.py

--- minified.py
+++ minified.py
@@ -1,15 +1,11 @@
 import flask;from googlesearch import search;import aiofiles;app = flask.Flask("Meta Search")
-async def write(thing):
-    async with aiofiles.open("log.log","a") as w:
-        w.write(thing)
-def run():
-    app.run(host="0.0.0.0",port=80)
+async def write(thing):async with aiofiles.open("log.log","a") as w:w.write(thing)
+def run():app.run(host="0.0.0.0",port=80)
 @app.route('/',methods=["POST","GET"])
 async def main():
     try:
         query = flask.request.headers.get("search")
-        if query == None:
-            return {"Result":[None]},400
+        if query == None:return {"Result":[None]},400
     except KeyError:
         return {"Result":[None]},400
     else:
